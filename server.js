bd5f995f85f35f0a1c13112fe1adc4589d2f9618
pedrois28 <94402107+pedrois28@users.noreply.github.com>
2025-11-04 12:34:48 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const { Server } = require("socket.io");
+const PORT = process.env.PORT || 3000;
+const io = new Server(PORT, { cors: { origin: "*" } });
+
+const clients = {};
+
+io.on("connection", (socket) => {
+  console.log("🔗 Nova ESP conectada:", socket.id);
+
+  socket.on("registrar", (id) => {
+    clients[id] = socket.id;
+    console.log(`✅ Registrado dispositivo: ${id}`);
+  });
+
+  socket.on("comando", (data) => {
+    const destinoSocket = clients[data.destino];
+    if (destinoSocket) {
+      io.to(destinoSocket).emit("acao", data.comando);
+    }
+  });
+
+  socket.on("disconnect", () => console.log("❌ ESP desconectada:", socket.id));
+});
+
+console.log(`🚀 Servidor rodando na porta ${PORT}`);
